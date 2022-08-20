90d41ade402bbef53bb545bb3bf79a360019270c
avaneesh-chandrashekhar <85017927+avaneesh-chandrashekhar@users.noreply.github.com>
2022-08-20 15:46:48 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,32 @@
+user_name = localStorage.getItem("Username");
+      document.getElementById("welcome_user_name").innerHTML = "Welcome "+user_name+"!";
+
+      function addroom() {
+             room_name = document.getElementById("room_name").value;
+
+             firebase.database().ref("/").child(room_name).update({
+                  purpose: "Adding Room Name"
+            });
+    
+            localStorage.setItem("Roomname",room_name);
+        
+            window.location = "kwitter_page.html";
+      }
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+                  console.log("room_name - " + Room_names);
+            row = "<div class='room_name' id="+Room_names+" onclick='redirectToroomname(this.id)'>#"+Room_names+"</div><hr>";
+            document.getElementById("output").innerHTML += row;
+      });});}
+getData();
+function redirectToroomname(name){
+      console.log(name);
+      localStorage.setItem("Roomname",name);
+      window.location = "kwitter_page.html";
+}
+function logout() {
+      localStorage.removeItem("Username");
+      localStorage.removeItem("Roomname");
+      window.location = "index.html";
+}
